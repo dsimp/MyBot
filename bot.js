d3cd5f838435c183901df2873951136278a8979e
dsimp <55302995+dsimp@users.noreply.github.com>
2021-10-29 00:07:06 -0500

    checkout

--- bot.js
+++ bot.js
@@ -10,7 +10,19 @@ async function givePage() {
 
 async function addToCart(page) {
   await page.goto(product_url);
-  await page.click();
+  await page.select(".gl-dropdown-native_select-element", "GW3355_620");
+  await page.click("button[data-auto-id='ys-add-to-bag-btn']", (elem) =>
+    elem.click()
+  );
+  await page.waitForSelector("a[data-auto-id='yeezy-mini-basket']");
+  await page.click("a[data-auto-id='yeezy-mini-basket']", (elem) =>
+    elem.click()
+  );
+  await page.click(
+    "button[data-auto-id='glass-checkout-button-bottom']",
+    (elem) => elem.click()
+  );
+  await page.waitForNavigation();
 }
 
 async function checkout() {
